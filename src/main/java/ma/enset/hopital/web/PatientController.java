f0ded18ec9ab5bd303e50317d49b934da864914d
Ouma49 <192384810+Ouma49@users.noreply.github.com>
2025-04-14 17:12:00 +0100

    Initial commit avec email GitHub noreply

---
+++ src/main/java/ma/enset/hopital/web/PatientController.java
@@ -0,0 +1,42 @@
+package ma.enset.hopital.web;
+
+import lombok.AllArgsConstructor;
+import ma.enset.hopital.entities.Patient;
+import ma.enset.hopital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class PatientController {
+
+    private PatientRepository patientRepository;
+@GetMapping("/index")
+
+ public String index(Model model, @RequestParam(name="page",defaultValue = "0")int page,
+                                  @RequestParam(name = "size",defaultValue ="4") int size,
+                                  @RequestParam(name= "keyword",defaultValue = "") String kw
+){
+    //List<Patient> patientList = patientRepository.findAll();
+    // model.addAttribute("listPatients", patientList);
+    Page<Patient> pagePatients = patientRepository.findByNomContains(kw,PageRequest.of(page, size)); //je veut 4 patient a partir de la page 0
+    model.addAttribute("listPatients",pagePatients.getContent());
+    model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
+    model.addAttribute("currentPage",page);
+    model.addAttribute("keyword",kw);
+    return "patients";
+ }
+ @GetMapping("/delete")
+  public String delete(Long id, String keyword, int page){
+    patientRepository.deleteById(id);
+    return "redirect:/index?page="+page+"&keyword="+keyword;
+  }
+
+}
